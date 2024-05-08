6a777626a941ad3af537be36cc18c8bfd7fd1df1
dee chao <chaoserady@gmail.com>
2024-05-08 01:58:25 -0500

    testing scrolling issues

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./header/Header";
 import Screen from "./screens/Screen";
+import Footer from "./footer/Footer";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Screen />
+      <Footer />
     </div>
   );
 }
